1bdae1a2f21fbb72c6fb1017b6ee7e3e0809fee6
songsz <song.shangzhi3@neolink.com>
2024-01-03 14:51:33 +0800

    peer ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,87 @@
+'use client';
+import {Peer} from 'peerjs'
 import Image from 'next/image'
+import {useCallback, useEffect, useState} from "react";
+import * as fs from "fs";
+
+const mySelf = 'mySelf'
+const others = 'others'
 
 export default function Home() {
+  const [id, setId] = useState<string>('')
+  const [otherId, setOtherId] = useState<string>('')
+  const [peer, setPeer] = useState<Peer>()
+  const [connect, setConnect] = useState<any>()
+  const [messages, setMessages] = useState<any[]>([]);
+  const [sendMsg, setSendMsg] = useState<string>()
+  const startPeer = useCallback(() => {
+    if (!id) return
+    const _peer = new Peer(id);
+    _peer.on('open', () => {
+      console.log(`peer open: id=${_peer.id}`)
+    })
+    setPeer(_peer)
+  }, [id, setPeer])
+
+  useEffect(() => {
+    if (!peer) return
+    peer.on('connection', (conn) => {
+      console.log('connection')
+      conn.on('data', (data) => {
+        console.log('conn.on: data', data)
+        setMessages(prevState => [...prevState, {from: others, msg: data}])
+      })
+    })
+  }, [peer, setMessages])
+
+  const startConnect = useCallback(() => {
+    if (!peer ||!otherId) return
+    const conn = peer.connect(otherId)
+    conn.on('open', () => {
+      console.log(`连接到 ${otherId}`)
+      conn.send(`hello, I'm ${peer.id}`)
+    })
+    setConnect(conn)
+  }, [peer, otherId, setConnect])
+  const send = useCallback(() => {
+    if (!connect || !sendMsg) return
+    connect.send(sendMsg)
+    setMessages(prevState => [...prevState, {from: mySelf, msg: sendMsg}])
+    setSendMsg('')
+  }, [connect, sendMsg, setMessages, setSendMsg])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="">
+      <br/>
+      我的id:<input value={id} onChange={(e) => setId(e.target.value)} placeholder={'Peer ID'}/>
+      <button onClick={startPeer}>开启服务</button>
+      <br/>
+      连接id: <input value={otherId} onChange={(e) => setOtherId(e.target.value)} placeholder={'other Peer ID'}/>
+      <button onClick={startConnect}>连接</button>
+      <br/> <br/>
+      消息列表:
+      <div className="" style={{
+        background: "rgb(244 243 244)",
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        {messages.map((msg, index) => (
+          msg.from === mySelf? (
+            <p
+              style={{
+                alignSelf: "flex-end",
+              }}
+              key={index}>{msg.msg}: {id}</p>
+        ) : (
+            <p
+              style={{
+                alignSelf: "flex-start",
+              }}
+              key={index}>{otherId}: {msg.msg}</p>
+          )))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <br/> <br/>
+      <textarea value={sendMsg} onChange={(e) => setSendMsg(e.target.value)} placeholder="发送消息"/>
+      <button onClick={send}>发送</button>
     </main>
   )
 }
